0e413c8ceafbbd8a3657e003976024d2cf8a7740
Bumblebee2019 <daria.senchylova@gmail.com>
2024-11-30 19:42:09 -0500

    Removed comment; checking that committing code works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+const initialGoals = [
+  {
+    name: "reading",
+    time: 60,
+    image:
+      "https://images.pexels.com/photos/159866/books-book-pages-read-literature-159866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    name: "working out",
+    time: 30,
+    image:
+      "https://images.pexels.com/photos/703014/pexels-photo-703014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+  {
+    name: "studying React",
+    time: 60,
+    image:
+      "https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header />
+      <div className="app">
+        <div className="sidebar">
+          <GoalList />
+
+          <AddGoalForm />
+          <button className="button">Add Goal</button>
+        </div>
+        <div className="main-content">
+          <UpdateGoalForm />
+        </div>
+      </div>
+    </>
+  );
+}
+function Header() {
+  return <div className="header">Goal Tracker</div>;
+}
+function Goal({ goal }) {
+  return (
+    <li>
+      <img src={goal.image} alt={goal.name} />
+      <h3>{goal.name}</h3> <button className="button">Select</button>
+    </li>
+  );
+}
+function GoalList() {
+  return (
+    <div>
+      <ul>
+        {" "}
+        {initialGoals.map((item) => (
+          <Goal goal={item} />
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
+function AddGoalForm() {
+  return (
+    <form className="form-add-friend">
+      Goal name: <input />
+      Motivating image: <input />
+      <button className="button">Add</button>
+    </form>
+  );
+}
+function UpdateGoalForm() {
+  return (
+    <form className="form-split-bill">
+      Your ____ form is <input /> minutes / day
+      <button className="button">Update</button>
+    </form>
+  );
+}
